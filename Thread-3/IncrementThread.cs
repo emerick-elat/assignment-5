0e9d108b60aec2b1d6d63d69eb0df363f3969c5a
Aymerick Fikeuelat <Aymerick.Fikeuelat@ventionteams.com>
2024-10-01 16:29:41 +0300

    subtask 3

---
+++ Thread-3/IncrementThread.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thread_3
+{
+    internal class IncrementThread
+    {
+        public int Value { get; set; }
+
+        public IncrementThread()
+        {
+            Value = 0;
+        }
+
+        public void Increment(int targetNumber)
+        {
+            if (Value <= targetNumber)
+            {
+                Value++;
+            }
+        }
+    }
+}
--- Thread-3/Program.cs
+++ Thread-3/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Threads_3
+﻿using Thread_3;
+
+namespace Threads_3
 {
     internal class Program
     {
@@ -10,6 +12,22 @@
             int.TryParse(Console.ReadLine(), out numberOfThreads);
             Console.WriteLine("Please enter the Target Number");
             int.TryParse(Console.ReadLine(), out targetNumber);
+
+            Thread[] threads = new Thread[numberOfThreads];
+
+            for (int i = 0; i < threads.Length; i++)
+            {
+                IncrementThread iThread = new IncrementThread();
+                threads[i] = new Thread(new ThreadStart(() => { 
+                    iThread.Increment(targetNumber);
+                }));
+                threads[i].Start();
+            }
+
+            foreach (Thread thread in threads)
+            {
+                thread.Join();
+            }
         }
     }
 }
